8f2d74b02576691aeb8300c5fdb4a4b934af1cfc
MWDelaney <michael@michaeldelaney.me>
2024-02-05 12:17:47 -0500

    Almost working, still needs weapong dislodge/retrieve

--- src/assets/scripts/webcomponents/sword-fight.js
+++ src/assets/scripts/webcomponents/sword-fight.js
@@ -3,6 +3,12 @@ class SwordFight extends HTMLElement {
   constructor() {
     super();
     this.attrs = {};
+
+    // Load my character
+    this.myCharacter = require("../../../data/humanFighter.json");
+
+    // Load the opponent's character
+    this.opponentsCharacter = require("../../../data/evilHumanFighter.json");
   }
 
   connectedCallback() {
@@ -32,32 +38,29 @@ class SwordFight extends HTMLElement {
    * handleMoveClick
    */
   handleMoveClick = (e) => {
-    let move = e.target;
-    let myMove = move.getAttribute("data-id");
+    if(e.target.classList.contains("button-move")) {
+      let move = e.target;
+      let myMove = move.getAttribute("data-id");
 
-            /**
-             * Random opponent move
-             */
-            let opponentsCharacter = require("../../../data/humanFighter.json");
-            let results = opponentsCharacter.results;
+              /**
+               * Random opponent move
+               */
 
-            // Get the result id from the #sword-fight element
-            let lastResult = this.getAttribute("data-result");
+              // Get my character's result from the previous round, since that's what will restrict the opponent's moves
+              let lastResult = this.getAttribute("data-result");
 
-            // Look up the results table for the current result
-            let result = results.find(result => result.id == lastResult);
+              // Look up the results table for the current result
+              let result = this.opponentsCharacter.results.find(result => result.id == lastResult);
 
-            // Get the opponent's available moves
-            let moves = this.filterMoves(opponentsCharacter.moves, result);
+              // Get the opponent's available moves
+              let moves = this.filterMoves(this.opponentsCharacter.moves, result);
 
-            // Get a random move from the opponent's moves
-            let randomMove = moves[Math.floor(Math.random() * moves.length)];
+              // Get a random move from the opponent's moves
+              let opponentMove = moves[Math.floor(Math.random() * moves.length)].id;
 
-            // Set the opponent's move to the random move's id
-            let opponentMove = randomMove.id
-
-    // Set up the game with the new moves
-    this.setUpGame(myMove, opponentMove);
+      // Set up the game with the new moves
+      this.setUpGame(myMove, opponentMove);
+    }
   }
 
   /**
@@ -66,43 +69,39 @@ class SwordFight extends HTMLElement {
    */
   setUpGame = (myMove = "62", opponentMove = "62") => {
 
+    // Log the characters
+    console.log("My Character: ", this.myCharacter.name);
+    console.log("Opponent's Character: ", this.opponentsCharacter.name);
+
+    // Log the moves
+    console.log("My Move: ", myMove);
+    console.log("Opponent's Move: ", opponentMove);
+
     /**
      * Set up your character and results
+     *
+     * In this game, your moves are come from your character's data, and your outcomes and results come from your opponent's character's data.
      */
-    // Get the character data
-    const character = require("../../../data/humanFighter.json");
-    // Get the opponent's character data
-    let opponentsCharacter = require("../../../data/humanFighter.json");
 
-    // Get the outcome tables from the character data
-    let outcomes = character.tables;
-    let results = character.results;
 
     // Get the results of this round
-    let outcome = this.getOutcome(myMove, opponentMove, outcomes);
-    let result = this.getResult(outcome, results);
-
-    // Get the opponent's outcome tables from the character data
-    let opponentsOutcomes = opponentsCharacter.tables;
-    let opponentsResults = opponentsCharacter.results;
+    let myOutcome = this.getOutcome(myMove, opponentMove, this.opponentsCharacter.tables);
+    let myResult = this.getResult(myOutcome, this.opponentsCharacter.results);
 
     // Get the opponent's result of this round
-    let opponentsOutcome = this.getOutcome(opponentMove, myMove, opponentsOutcomes);
-    let opponentsResult = this.getResult(opponentsOutcome, opponentsResults);
-
-    // console.log("Result: ", result);
-    // console.log("Opponent's Result: ", opponentsResult);
+    let opponentsOutcome = this.getOutcome(opponentMove, myMove, this.myCharacter.tables);
+    let opponentsResult = this.getResult(opponentsOutcome, this.myCharacter.results);
 
     // Set an attribute on the sword-fight element with the current result's ID for reference next round
-    this.setAttribute("data-result", result.id);
+    this.setAttribute("data-result", myResult.id);
 
     // Set an attribute on the sword-fight element with the opponent's result for the next round
-    this.setAttribute("data-opponent-result", result.id);
+    this.setAttribute("data-opponent-result", myResult.id);
 
     /**
      * Log the last round
      */
-    this.logRound(myMove, opponentMove, character, opponentsCharacter, result, opponentsResult);
+    //this.logRound(myMove, opponentMove, this.myCharacter.tables, this.opponentsCharacter.tables, myResult, opponentsResult);
 
 
     /**
@@ -111,7 +110,7 @@ class SwordFight extends HTMLElement {
 
     // Populate #theLastOutcome with the name of the outcome
     let viewName = document.getElementById("viewName");
-    viewName.innerHTML = result.name;
+    viewName.innerHTML = myResult.name;
 
 
     /**
@@ -126,30 +125,34 @@ class SwordFight extends HTMLElement {
      * Set up the moves list
      */
 
-    // Filter the moves to only include the moves that are available to the character
-    let moves = this.filterMoves(character.moves, result);
+    // Get all my character's moves
+    let myMoves = this.myCharacter.moves;
+
+    // Get the moves filtered by the result of the last round
+    let myFilteredMoves = this.filterMoves(this.myCharacter.moves, opponentsResult);
 
     // Get the character's moves and create buttons in #characterSheetMovesList with data-attributes for tag, range, type, and id
     let characterSheetMovesList = document.getElementById("characterSheetMovesList");
     characterSheetMovesList.innerHTML = "";
-    moves.forEach(move => {
+    myMoves.forEach(move => {
       let button = document.createElement("button");
-      button.setAttribute("data-tag", move.tag);
-      button.setAttribute("data-range", move.range);
-      button.setAttribute("data-type", move.type);
-      button.setAttribute("data-id", move.id);
+      // If this move is available, add all the data-attributes
+      if(myFilteredMoves.find(filteredMove => filteredMove.id == move.id)) {
+        button.setAttribute("data-tag", move.tag);
+        button.setAttribute("data-range", move.range);
+        button.setAttribute("data-type", move.type);
+        button.setAttribute("data-id", move.id);
+        button.classList.add("button-move");
 
-      // Disable this move if it's out of range
-      if(move.range != result.range) {
-        button.disabled = true;
+      } else {
+        // If this move is not available, add a disabled attribute
+        button.setAttribute("disabled", "disabled");
       }
 
-      // result.restrict is an array of move types that are restricted. If this move is restricted, disable it
-      if(result.restrict.includes(move.type)) {
-        button.disabled = true;
-      }
-
+      // Set the button's innerHTML to the move's tag and name
       button.innerHTML = move.tag + " - " + move.name;
+
+      button.innerHTML += `<span class="badge badge-primary badge-pill">` + move.id + `</span>`;
 
       // Wrap each button in an li
       let li = document.createElement("li");
@@ -167,10 +170,13 @@ class SwordFight extends HTMLElement {
   filterMoves = (moves, result) => {
     // Filter the moves to only include the moves that are available to the character
     moves = moves.filter(move => {
+
       // If the move's range is not the same as the result's range, filter it out
       if(move.range != result.range) {
         return false;
       }
+
+      // Log the array of restricted move types
 
       // If the move's type is in the result's restrict array, filter it out
       if(result.restrict.includes(move.type)) {
@@ -205,10 +211,24 @@ class SwordFight extends HTMLElement {
     // Get the name of the result of the opponent's move
     let opponentResultName = opponentResult.name;
 
+    // Concatenate myResult.restrict into <strong> tags
+    let myRestrictions = "";
+    myResult.restrict.forEach(restriction => {
+      myRestrictions += "<strong>" + restriction + "</strong> ";
+    });
+
+    // Concatenate opponentResult.restrict into <strong> tags
+    let opponentRestrictions = "";
+    opponentResult.restrict.forEach(restriction => {
+      opponentRestrictions += "<strong>" + restriction + "</strong> ";
+    });
+
     // Create a list item in the log
     let log = document.getElementById("gameLogList");
     let li = document.createElement("li");
-    li.innerHTML = `<span class="game-log-my-move">` + myMoveName + `</span> vs <span class="game-log-opponents-move">` + opponentMoveName + `</span> <p>Resulting in: <span class="game-log-opponent-sees-result">` + myResultName + `</span> <span class="game-log-my-sees-result">` + opponentResultName + `</span></p>`;
+
+    li.innerHTML += `<div class="alert alert-primary">You did a ` + myMoveName + ` and your opponent was ` + myResultName + `<p>Your oponenent cannot do any ` + myRestrictions + `</div>`;
+    li.innerHTML += `<div class="alert alert-warning">Your opponent did a ` + opponentMoveName + ` and you were ` + opponentResultName + `<p>You cannot do any ` + opponentRestrictions + `</div>`;
 
     // Append the LI to the log
     log.appendChild(li);
@@ -224,15 +244,15 @@ class SwordFight extends HTMLElement {
    * @param {object} table - The outcome tables for my move
    * @returns {string} - The outcome of the current round
    */
-  getOutcome = (myMove, opponentsMove, outcomes) => {
-    // Initialize the outcome variable
-    let outcome = "";
-
-    // Get the outcome table for the current move
-    let table = outcomes.find(outcome => outcome.id == myMove).outcomes[0];
+  getOutcome = (myMove, opponentsMove, tables) => {
+    // Get the outcome array for the current moves
+    let table = tables.find(table => table.id == myMove).outcomes[0];
 
     // Look the opponent's move in my move's outcome table
-    outcome = table[opponentsMove];
+    let outcome = table[opponentsMove];
+
+    // Log the outcome
+    console.log("Outcome: ", outcome);
 
     // Return the outcome (number) as a string
     return outcome;
@@ -246,9 +266,10 @@ class SwordFight extends HTMLElement {
    * @returns {obj} - The result of the current round
    */
   getResult = (outcome, results) => {
-    // Initialize the result variable
-    let result = {};
-    result = results.find(result => result.id == outcome);
+    let result = results.find(result => result.id == outcome);
+
+    // Log the result
+    console.log("Result: ", result);
 
     return result;
   }
